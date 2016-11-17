b655b5eb53d0c92636939620dfc6135e1effb454
Jonas Kello <jonas.kello@divid.se>
2016-11-17 11:30:49 +0100

    Add example

---
+++ example/main.js
@@ -0,0 +1,41 @@
+/**
+ * @author jonaskello / https://github.com/jonaskello
+ * Simple example that renders a scene with a cube to a PNG image file.
+ */
+const THREE = require("three");
+const SoftwareRenderer = require("../software-renderer");
+const PNG = require("pngjs").PNG;
+const fs = require("fs");
+
+// Build scene with cube
+const width = 1024;
+const height = 768;
+const camera = new THREE.PerspectiveCamera(75, width / height, 1, 10000);
+camera.position.z = 500;
+const scene = new THREE.Scene();
+const geometry = new THREE.BoxGeometry(200, 200, 200);
+const material = new THREE.MeshBasicMaterial({color: 0xff0000});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Rotate the cube a bit
+mesh.rotation.x += 0.5;
+mesh.rotation.y += 0.6;
+
+// Render into pixels-array (RGBA)
+const renderer = new SoftwareRenderer();
+renderer.setSize(width, height);
+var imagedata = renderer.render(scene, camera);
+const theData = imagedata.data;
+
+// Create a PNG from the pixels array (RGBA)
+const png = new PNG({
+  width: width,
+  height: height,
+  filterType: -1
+});
+png.data = theData;
+if (!fs.existsSync("temp")) {
+  fs.mkdirSync("temp");
+}
+png.pack().pipe(fs.createWriteStream("temp/example.png"));
